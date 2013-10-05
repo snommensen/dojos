409be9d0ce3ee7e21cc5d38e05246ab74e295c6d
Sönke Nommensen <snommensen@gmail.com>
2013-10-05 13:54:16 +0200

    Business Tasks solution.

--- business-tasks/src/main/java/BusinessTasks.java
+++ business-tasks/src/main/java/BusinessTasks.java
@@ -1,7 +1,19 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class BusinessTasks {
 
     public String getTask(String[] list, int n) {
-        // TODO http://community.topcoder.com/stat?c=problem_statement&pm=1585&rd=6535
-        return null;
+        LinkedList<String> internalList = new LinkedList<>(Arrays.asList(list));
+        int idx = 0;
+        while (internalList.size() > 1) {
+            idx = calculateListIndex(internalList.size(), idx + n);
+            internalList.remove(idx);
+        }
+        return internalList.get(0);
+    }
+
+    int calculateListIndex(int listSize, int n) {
+        return (n - 1) % listSize;
     }
 }
