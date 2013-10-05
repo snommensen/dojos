04542d4793c3870ff9891c0c4fcd06c95331f306
Sönke Nommensen <snommensen@gmail.com>
2013-10-05 21:01:29 +0200

    Revert "Revert "Added new problem statement.""
    This reverts commit 7ce34c3b4683a342be5b27e552f18ded1bc614f2.

---
+++ little-elephant/src/main/java/LittleElephantAndBalls.java
@@ -0,0 +1,11 @@
+
+/**
+ * Problem statement: http://community.topcoder.com/stat?c=problem_statement&pm=12758&rd=15704
+ */
+public class LittleElephantAndBalls {
+
+    public int getNumber(String s) {
+        return 0;
+    }
+}
+
