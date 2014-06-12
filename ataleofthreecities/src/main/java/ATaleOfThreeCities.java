a5a2579aca2dd6ea7f1f1404914114973f4efe7c
Sönke Nommensen <snommensen@gmail.com>
2014-06-12 14:34:48 +0200

    First solution.

--- ataleofthreecities/src/main/java/ATaleOfThreeCities.java
+++ ataleofthreecities/src/main/java/ATaleOfThreeCities.java
@@ -7,6 +7,35 @@ public class ATaleOfThreeCities {
     public double connect(int[] ax, int[] ay,
                           int[] bx, int[] by,
                           int[] cx, int[] cy) {
-        return 0.0;
+
+        double ab, ac, bc;
+        ab = ac = bc = Double.MAX_VALUE;
+
+        for (int i = 0; i < ax.length; i++) {
+            for (int j = 0; j < bx.length; j++) {
+                double d = euclidianDistance(ax[i], ay[i], bx[j], by[j]);
+                ab = Math.min(d, ab);
+            }
+        }
+
+        for (int i = 0; i < ax.length; i++) {
+            for (int j = 0; j < cx.length; j++) {
+                double d = euclidianDistance(ax[i], ay[i], cx[j], cy[j]);
+                ac = Math.min(d, ac);
+            }
+        }
+
+        for (int i = 0; i < bx.length; i++) {
+            for (int j = 0; j < cx.length; j++) {
+                double d = euclidianDistance(bx[i], by[i], cx[j], cy[j]);
+                bc = Math.min(bc, d);
+            }
+        }
+
+        return ab + ac + bc - Math.max(Math.max(ab, ac), bc);
+    }
+
+    private double euclidianDistance(int x1, int x2, int y1, int y2) {
+        return Math.sqrt(Math.pow(x1 - y1, 2) + Math.pow(x2 - y2, 2));
     }
 }
--- ataleofthreecities/src/test/groovy/ATaleOfThreeCitiesSpec.groovy
+++ ataleofthreecities/src/test/groovy/ATaleOfThreeCitiesSpec.groovy
@@ -12,7 +12,7 @@ class ATaleOfThreeCitiesSpec extends Specification {
         aTaleOfThreeCities = ATaleOfThreeCities.make()
     }
 
-    def "the correct minimal costs for a tunnel are calculated"() {
+    def "the correct minimal costs for two tunnels are calculated"() {
         expect:
         aTaleOfThreeCities.connect(
                 ax as int[], ay as int[],
@@ -20,7 +20,9 @@ class ATaleOfThreeCitiesSpec extends Specification {
                 cx as int[], cy as int[]) == result
 
         where:
-        ax        | ay        | bx     | by     | cx     | cy      | result
-        [0, 0, 0] | [0, 1, 2] | [2, 3] | [1, 1] | [1, 5] | [3, 28] | 3.414213562373095
+        ax                                | ay                                 | bx                                       | by                                       | cx                                       | cy                                        | result
+        [0, 0, 0]                         | [0, 1, 2]                          | [2, 3]                                   | [1, 1]                                   | [1, 5]                                   | [3, 28]                                   | 3.414213562373095
+        [-2, -1, 0, 1, 2]                 | [0, 0, 0, 0, 0]                    | [-2, -1, 0, 1, 2]                        | [1, 1, 1, 1, 1]                          | [-2, -1, 0, 1, 2]                        | [2, 2, 2, 2, 2]                           | 2.0
+        [4, 5, 11, 21, 8, 10, 3, 9, 5, 6] | [12, 8, 9, 12, 2, 3, 5, 7, 10, 13] | [34, 35, 36, 41, 32, 39, 41, 37, 39, 50] | [51, 33, 41, 45, 48, 22, 33, 51, 41, 40] | [86, 75, 78, 81, 89, 77, 83, 88, 99, 77] | [10, 20, 30, 40, 50, 60, 70, 80, 90, 100] | 50.323397776611024
     }
 }
