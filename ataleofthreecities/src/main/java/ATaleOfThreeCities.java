8460f2b69b621b5f07663debee3257ac5dba0a7f
Sönke Nommensen <snommensen@gmail.com>
2014-06-12 14:35:39 +0200

    Corrected typo.

--- ataleofthreecities/src/main/java/ATaleOfThreeCities.java
+++ ataleofthreecities/src/main/java/ATaleOfThreeCities.java
@@ -13,21 +13,21 @@ public class ATaleOfThreeCities {
 
         for (int i = 0; i < ax.length; i++) {
             for (int j = 0; j < bx.length; j++) {
-                double d = euclidianDistance(ax[i], ay[i], bx[j], by[j]);
+                double d = euclideanDistance(ax[i], ay[i], bx[j], by[j]);
                 ab = Math.min(d, ab);
             }
         }
 
         for (int i = 0; i < ax.length; i++) {
             for (int j = 0; j < cx.length; j++) {
-                double d = euclidianDistance(ax[i], ay[i], cx[j], cy[j]);
+                double d = euclideanDistance(ax[i], ay[i], cx[j], cy[j]);
                 ac = Math.min(d, ac);
             }
         }
 
         for (int i = 0; i < bx.length; i++) {
             for (int j = 0; j < cx.length; j++) {
-                double d = euclidianDistance(bx[i], by[i], cx[j], cy[j]);
+                double d = euclideanDistance(bx[i], by[i], cx[j], cy[j]);
                 bc = Math.min(bc, d);
             }
         }
@@ -35,7 +35,7 @@ public class ATaleOfThreeCities {
         return ab + ac + bc - Math.max(Math.max(ab, ac), bc);
     }
 
-    private double euclidianDistance(int x1, int x2, int y1, int y2) {
+    private double euclideanDistance(int x1, int x2, int y1, int y2) {
         return Math.sqrt(Math.pow(x1 - y1, 2) + Math.pow(x2 - y2, 2));
     }
 }
