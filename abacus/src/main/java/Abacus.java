514b8fafeffbfa133ec3f36196bba211158d8c0e
Sönke Nommensen <snommensen@gmail.com>
2014-06-14 13:08:57 +0200

    Solution

--- abacus/src/main/java/Abacus.java
+++ abacus/src/main/java/Abacus.java
@@ -1,6 +1,37 @@
 public class Abacus {
 
-    String[] add(String[] original, int val) {
-        return new String[]{""};
+    public String[] add(String[] original, int val) {
+        int number = fromAbacus(original);
+        int sum = number + val;
+        return toAbacus(sum);
+    }
+
+    static int fromAbacus(String[] abacus) {
+        String number = "";
+        for (String s : abacus) {
+            int idx = s.lastIndexOf('-');
+            String n = s.substring(idx + 1);
+            number += n.length();
+        }
+        return Integer.parseInt(number);
+    }
+
+    static String[] toAbacus(int number) {
+        String n = "" + number;
+        String[] abacus = new String[n.length()];
+        int k = 0;
+        for (char c : n.toCharArray()) {
+            int i = Integer.parseInt("" + c);
+            int r = 9 - i;
+            String s = "";
+            for (int j = 0; j < r; j++)
+                s += "o";
+            s += "---";
+            for (int j = 0; j < i; j++)
+                s += "o";
+            abacus[k] = s;
+            k++;
+        }
+        return abacus;
     }
 }
--- abacus/src/test/groovy/AbacusSpec.groovy
+++ abacus/src/test/groovy/AbacusSpec.groovy
@@ -11,18 +11,91 @@ class AbacusSpec extends Specification {
         abacus.add(original as String[], val) == result
 
         where:
-        original         | val | result
+        original         | val    | result
         ["ooo---oooooo",
          "---ooooooooo",
          "---ooooooooo",
          "---ooooooooo",
          "oo---ooooooo",
-         "---ooooooooo"] | 5   | ["ooo---oooooo",
-                                  "---ooooooooo",
-                                  "---ooooooooo",
-                                  "---ooooooooo",
-                                  "o---oooooooo",
-                                  "ooooo---oooo"]
+         "---ooooooooo"] | 5      | ["ooo---oooooo",
+                                     "---ooooooooo",
+                                     "---ooooooooo",
+                                     "---ooooooooo",
+                                     "o---oooooooo",
+                                     "ooooo---oooo"]
+        ["ooo---oooooo",
+         "---ooooooooo",
+         "---ooooooooo",
+         "---ooooooooo",
+         "oo---ooooooo",
+         "---ooooooooo"] | 21     | ["oo---ooooooo",
+                                     "ooooooooo---",
+                                     "ooooooooo---",
+                                     "ooooooooo---",
+                                     "ooooooooo---",
+                                     "ooooooooo---"]
+        ["ooooooooo---",
+         "---ooooooooo",
+         "ooooooooo---",
+         "---ooooooooo",
+         "oo---ooooooo",
+         "---ooooooooo"] | 100000 | ["oooooooo---o",
+                                     "---ooooooooo",
+                                     "ooooooooo---",
+                                     "---ooooooooo",
+                                     "oo---ooooooo",
+                                     "---ooooooooo"]
+        ["o---oooooooo",
+         "---ooooooooo",
+         "---ooooooooo",
+         "---ooooooooo",
+         "---ooooooooo",
+         "---ooooooooo"] | 1      | ["---ooooooooo",
+                                     "ooooooooo---",
+                                     "ooooooooo---",
+                                     "ooooooooo---",
+                                     "ooooooooo---",
+                                     "ooooooooo---"]
+    }
+
+    def "an abacus string is correctly parsed"() {
+        expect:
+        Abacus.fromAbacus(a as String[]) == number
+
+        where:
+        a                | number
+        ["ooo---oooooo",
+         "---ooooooooo",
+         "---ooooooooo",
+         "---ooooooooo",
+         "oo---ooooooo",
+         "---ooooooooo"] | 699979
+        ["ooo---oooooo",
+         "---ooooooooo",
+         "---ooooooooo",
+         "---ooooooooo",
+         "o---oooooooo",
+         "ooooo---oooo"] | 699984
+    }
+
+    def "an abacus representation is correctly constructed from an interger"() {
+        expect:
+        Abacus.toAbacus(number) == a
+
+        where:
+        number | a
+        699979 | ["ooo---oooooo",
+                  "---ooooooooo",
+                  "---ooooooooo",
+                  "---ooooooooo",
+                  "oo---ooooooo",
+                  "---ooooooooo"]
+        699984 | ["ooo---oooooo",
+                  "---ooooooooo",
+                  "---ooooooooo",
+                  "---ooooooooo",
+                  "o---oooooooo",
+                  "ooooo---oooo"]
     }
 
 }
