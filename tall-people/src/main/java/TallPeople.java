134c8754e5295b812e9f1a42cdde80fbf41c44be
Soenke Nommensen <snommensen@gmail.com>
2017-11-22 12:21:35 +0100

    Played around with IntStreams

--- tall-people/src/main/java/TallPeople.java
+++ tall-people/src/main/java/TallPeople.java
@@ -1,6 +1,7 @@
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.LinkedList;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * Problem statement:
@@ -9,48 +10,80 @@ import java.util.LinkedList;
 public class TallPeople {
 
     public int[] getPeople(String[] people) {
+        RowsAndCols rowsAndCols = parse(people);
         return new int[]{
-            tallestOfTheShortest(people),
-            shortestOfTheTallest(people)
+            tallestOfTheShortest(rowsAndCols.getRows()),
+            shortestOfTheTallest(rowsAndCols.getCols())
         };
     }
 
-    int tallestOfTheShortest(String[] people) {
+    private int tallestOfTheShortest(IntStream[] rows) {
         HashSet<Integer> shortestPerRow = new HashSet<>();
-        for (String row : people) {
-            LinkedList<Integer> heights = parseRow(row);
-            shortestPerRow.add(Collections.min(heights));
-        }
+        Arrays.stream(rows)
+            .forEach(row -> row
+                .min()
+                .ifPresent(shortestPerRow::add));
         return Collections.max(shortestPerRow);
     }
 
-    int shortestOfTheTallest(String[] people) {
+    private int shortestOfTheTallest(IntStream[] cols) {
         HashSet<Integer> tallestPerColumn = new HashSet<>();
-        for (HashSet<Integer> column : extractColumns(people)) {
-            tallestPerColumn.add(Collections.max(column));
-        }
+        Arrays.stream(cols)
+            .forEach(col -> col
+                .max()
+                .ifPresent(tallestPerColumn::add));
         return Collections.min(tallestPerColumn);
     }
 
-    private HashSet<Integer>[] extractColumns(String[] people) {
-        final int noOfColumns = people[0].split(" ").length;
-        HashSet<Integer>[] columns = new HashSet[noOfColumns];
-        for (String row : people) {
-            LinkedList<Integer> heights = parseRow(row);
-            for (int i = 0; i < noOfColumns; i++) {
-                if (columns[i] == null) columns[i] = new HashSet<>();
-                columns[i].add(heights.get(i));
-            }
-        }
-        return columns;
+    private RowsAndCols parse(String[] people) {
+        final int noOfRows = people.length;
+        final int noOfCols = people[0].split(" ").length;
+
+        int[][] rows = new int[noOfRows][noOfCols];
+        IntStream.range(0, noOfRows)
+            .forEach(i -> {
+                rows[i] = parseRow(people[i]).toArray();
+            });
+
+        int[][] cols = new int[noOfCols][noOfRows];
+        IntStream.range(0, noOfCols)
+            .forEach(j -> {
+                IntStream.range(0, noOfRows)
+                    .forEach(k -> {
+                        cols[j][k] = rows[k][j];
+                    });
+            });
+
+        return new RowsAndCols(rows, cols);
     }
 
-    private LinkedList<Integer> parseRow(String row) {
-        LinkedList<Integer> heightsAsIntegers = new LinkedList<>();
-        for (String height : row.split(" ")) {
-            heightsAsIntegers.add(Integer.valueOf(height));
+    private IntStream parseRow(String row) {
+        return Arrays.stream(row.split(" "))
+            .mapToInt(Integer::valueOf);
+    }
+
+    private static class RowsAndCols {
+        private int[][] rows;
+        private int[][] cols;
+
+        RowsAndCols(int[][] rows, int[][] cols) {
+            this.rows = rows;
+            this.cols = cols;
         }
-        return heightsAsIntegers;
+
+        IntStream[] getRows() {
+            return getIntStreams(rows);
+        }
+
+        IntStream[] getCols() {
+            return getIntStreams(cols);
+        }
+
+        private IntStream[] getIntStreams(int[][] ints) {
+            IntStream[] result = new IntStream[ints.length];
+            IntStream.range(0, ints.length)
+                .forEach(i -> result[i] = Arrays.stream(ints[i]));
+            return result;
+        }
     }
 }
-
