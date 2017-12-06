9430c02a7968197a0e21a90735eb7bd3845547c7
Soenke Nommensen <soenke@element.in>
2017-12-06 17:07:00 +0100

    Implement algorithm

--- ellys-chocolates/src/main/kotlin/ellys_chocolates/EllysChocolates.kt
+++ ellys-chocolates/src/main/kotlin/ellys_chocolates/EllysChocolates.kt
@@ -1,3 +1,44 @@
 package ellys_chocolates
 
-class EllysChocolates
+class EllysChocolates {
+
+    fun getCount(P: Int, K: Int, N: Int): Int {
+        var chocolates = 0
+        var amountOfMoney = N
+        var exchanges = 1
+
+        while (canBuy(amountOfMoney, P)) {
+            val buyResult = buy(amountOfMoney, P, chocolates)
+            amountOfMoney = buyResult.first
+            chocolates = buyResult.second
+
+            if (canExchange(chocolates, exchanges, K)) {
+                val exchangeResult = exchange(chocolates, exchanges)
+                chocolates = exchangeResult.first
+                exchanges = exchangeResult.second
+            }
+        }
+
+        return chocolates
+    }
+
+    private fun canBuy(amountOfMoney: Int, P: Int): Boolean = amountOfMoney >= P
+
+    private fun canExchange(chocolates: Int, exchanges: Int, K: Int): Boolean = chocolates % K == 0
+
+    private fun exchange(chocolates: Int, exchanges: Int): Pair<Int, Int> {
+        var chocolates1 = chocolates
+        var exchanges1 = exchanges
+        chocolates1 += 1
+        exchanges1 += 1
+        return Pair(chocolates1, exchanges1)
+    }
+
+    private fun buy(amountOfMoney: Int, price: Int, chocolates: Int): Pair<Int, Int> {
+        var amountOfMoney1 = amountOfMoney
+        var chocolates1 = chocolates
+        amountOfMoney1 -= price
+        chocolates1 += 1
+        return Pair(amountOfMoney1, chocolates1)
+    }
+}
--- ellys-chocolates/src/test/kotlin/ellys_chocolates/EllysChocolatesTest.kt
+++ ellys-chocolates/src/test/kotlin/ellys_chocolates/EllysChocolatesTest.kt
@@ -1,18 +1,32 @@
 package ellys_chocolates
 
 import org.junit.Test
-import kotlin.test.assertTrue
+import kotlin.test.assertEquals
 
 /*
  * https://community.topcoder.com/stat?c=problem_statement&pm=14681
  */
-
 class EllysChocolatesTest {
 
     val ellysChocolates = EllysChocolates()
 
     @Test
-    fun `can calculate optimal number`() {
-        assertTrue { true }
+    fun `Elly has 15 euros, one is sold for 1, 3 can be exchanged for a new one, she can get 15`() {
+        assertEquals(22, ellysChocolates.getCount(1, 3, 15))
+    }
+
+    @Test
+    fun `Elly has 823172 euros, one is sold for 666, 13 can be exchanged for a new one, she can get 1337`() {
+        assertEquals(1337, ellysChocolates.getCount(666, 13, 823172))
+    }
+
+    @Test
+    fun `Elly has 444 euros, one is sold for 666, 222 can be exchanged for a new one, she can get 0`() {
+        assertEquals(0, ellysChocolates.getCount(666, 222, 444))
+    }
+
+    @Test
+    fun `Elly has 1337 euros, one is sold for 41, 4 can be exchanged for a new one, she can get 42`() {
+        assertEquals(42, ellysChocolates.getCount(41, 4, 1337))
     }
 }
